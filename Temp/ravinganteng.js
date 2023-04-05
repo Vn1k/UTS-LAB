dc13bf117094a55a30ebad2a33c1bf48ce4bd536
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-04-05 16:14:57 +0700

    d

--- Temp/ravinganteng.js
+++
@@ -1,9 +0,0 @@
-let money = 0;
-
-const character = document.getElementById('avatar');
-
-character.addEventListener("click", function(){
-//    const randommoney = Math.floor(Math.random() * 10) + 1;        -->ini kalau mau yang di tambah angka random
-//    money += 1;           -->ini kalau perclick nambah 1 duit
-    console.log("money: " + money);
-});
